5c70d35ee1971a7818c97ed8b56bd31f119491d7
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 20:49:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ speak_to_grandma = "HUH?! SPEAK UP, SONNY!"
   if speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
 
-  elsif speak_to_grandma.uppercase 
+  elsif (.uppercasespeak_to_grandma)
     puts "NO, NOT SINCE 1938"
 
   end
