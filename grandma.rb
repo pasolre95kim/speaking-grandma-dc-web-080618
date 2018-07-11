e4b298e2cf1ddbe246224cc71f71ac59f50bbfb2
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 20:51:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma = 
+def speak_to_grandma = "HuH?! SPEAK UP, SONNY!"
   if speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
 
