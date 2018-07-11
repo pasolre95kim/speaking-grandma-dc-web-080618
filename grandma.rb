c67b31636642af65552228d79ee060248fec5f4d
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 20:59:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma (phrase)
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
 
-  else phrase == "HUH?! SPAK UP, SONNY!"
+  else phrase == "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
