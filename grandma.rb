cae05f58938c46804b2f72461a354499c86bf8ba
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 20:42:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -9,6 +9,8 @@ def speak_to_grandma (phrase)
 
   else
     puts "HUH?! SPEAK UP, SONNY!"
+
+  end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
